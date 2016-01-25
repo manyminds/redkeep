2d19be0ac364f872f1d29331f58661e58c9c9845
n.wagensonner <n.wagensonner@manyminds.de>
2016-01-25 19:14:41 +0100

    Run actual tests against the agent
    This now uses the same makefile and configuration
    for both travis and local dev environment

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 testenv:
-	mkdir -p tmpdb/data{0,1,2}
 	mkdir -p tmpdb/log
+	mkdir -p tmpdb/data0
+	mkdir -p tmpdb/data1
+	mkdir -p tmpdb/data2
 	mongod --dbpath tmpdb/data0 --port 30000 --replSet=testRs --noprealloc --oplogSize=5 --fork --logpath tmpdb/log/0.log && sleep 2
 	mongod --dbpath tmpdb/data1 --port 30001 --replSet=testRs --noprealloc --oplogSize=5 --fork --logpath tmpdb/log/1.log && sleep 2
 	mongod --dbpath tmpdb/data2 --port 30002 --replSet=testRs --noprealloc --oplogSize=5 --fork --logpath tmpdb/log/2.log && sleep 2
 	mongo --port 30000 --eval 'rs.initiate({"_id" : "testRs", members: [ {"host" : "127.0.0.1:30000", "_id" : 1}, {"host" : "127.0.0.1:30001", "_id" : 2}, {"host" : "127.0.0.1:30002", "_id" : 3 } ]})'
 
 clean:
-	ps ax | grep tmpdb | grep -v grep | awk '{print $$1}' | xargs -n1 kill
+	@-ps ax | grep tmpdb | grep -v grep | awk '{print $$1}' | xargs -n1 kill
 	rm -rf tmpdb/
--- tail_test.go
+++ tail_test.go
@@ -1,6 +1,10 @@
 package redkeep_test
 
 import (
+	"io/ioutil"
+	"log"
+	"time"
+
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
@@ -11,29 +15,81 @@ import (
 )
 
 var _ = Describe("Tail", func() {
-	/*
-	 *  var (
-	 *    running chan bool
-	 *  )
-	 *
-	 *  BeforeSuite(func() {
-	 *    file, err := ioutil.ReadFile(testConfiguration)
-	 *    Expect(err).ToNot(HaveOccurred())
-	 *    config, err := NewConfiguration(file)
-	 *    Expect(err).ToNot(HaveOccurred())
-	 *    agent, err := NewTailAgent(*config)
-	 *    if err != nil {
-	 *      log.Fatal(err)
-	 *    }
-	 *    running = make(chan bool)
-	 *    go agent.Tail(running, false)
-	 *  })
-	 *
-	 *  AfterSuite(func() {
-	 *    running <- false
-	 *  })
-	 *
-	 */
+	var (
+		running chan bool
+	)
+
+	BeforeSuite(func() {
+		file, err := ioutil.ReadFile(testConfiguration)
+		Expect(err).ToNot(HaveOccurred())
+		config, err := NewConfiguration(file)
+		Expect(err).ToNot(HaveOccurred())
+		agent, err := NewTailAgent(*config)
+		if err != nil {
+			log.Fatal(err)
+		}
+		running = make(chan bool)
+		go agent.Tail(running, false)
+	})
+
+	AfterSuite(func() {
+		running <- false
+	})
+
+	Context("Database testcases", func() {
+		var (
+			db *mgo.Session
+		)
+
+		BeforeEach(func() {
+			var err error
+			db, err = mgo.Dial("localhost:30000,localhost:30001,localhost:30002")
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("Should update infos on insert correctly", func() {
+
+			userID := bson.ObjectIdHex("56a65494b204ccd1edc0b055")
+			userRef := mgo.DBRef{
+				Database:   "testing",
+				Id:         userID,
+				Collection: "user",
+			}
+
+			db.DB("testing").C("user").Insert(
+				bson.M{
+					"_id":      userID,
+					"username": "naan",
+					"gender":   "male",
+					"name": bson.M{
+						"firstName": "Naan",
+						"lastName":  "Waana",
+					},
+				},
+			)
+
+			db.DB("testing").C("comment").Insert(
+				bson.M{
+					"text": "this is my first comment",
+					"user": userRef,
+				},
+			)
+
+			type comment struct {
+				Text string
+				User mgo.DBRef
+				Meta map[string]interface{}
+			}
+
+			actual := comment{}
+			time.Sleep(10 * time.Millisecond)
+			db.Copy().DB("testing").C("comment").Find(bson.M{}).One(&actual)
+
+			Expect(actual.Meta["username"]).To(Equal("naan"))
+			Expect(actual.Meta["gender"]).To(Equal("male"))
+		})
+	})
+
 	Context("Test HasKey", func() {
 		It("validate functionality", func() {
 			toTest := map[string]interface{}{
